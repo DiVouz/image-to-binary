e6051653081b30cc66f8aa67f7de78698ef3857e
Dimitris Vouzaxakis <divouz@outlook.com>
2019-02-05 18:42:18 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
-
+let img;
 
 function setup() {
-
+   createCanvas(600,300);
+   img = loadImage('img/test.png');
+  
+   image(img, 0, 0);
+   image(img, 300, 0);
 }
 
 function draw() {
-
+   
 }
