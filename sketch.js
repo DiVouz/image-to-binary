2e271494301516044b1ea84226531f317ff5c03b
Dimitris Vouzaxakis <divouz@outlook.com>
2019-02-05 18:46:50 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let img;
 
 function setup() {
    createCanvas(600,300);
-   img = loadImage('img/test.png');
+   img = loadImage('https://codropspz-tympanus.netdna-ssl.com/codrops/wp-content/uploads/2015/02/TextFill_image3.png');
   
    image(img, 0, 0);
    image(img, 300, 0);
